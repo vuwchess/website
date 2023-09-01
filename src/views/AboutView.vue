d2393d03f8a70a96033ad7798cd117aae45b5107
ElPlaso <plahsoekusay@gmail.com>
2023-09-01 15:14:29 +1200

    feat: update about us content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A social club for casual and competitive chess</h1>
+    <p>
+      The Victoria University of Wellington Chess Club is a place for chess enthusiasts to come together, share
+      their love for the game, and have a good time. No matter your skill level, you're invited to be a part of the
+      community.
+    </p>
+    <p>Our weekly meetups offer a space to enjoy casual over-the-board chess matches and work together on solving chess
+      puzzles. It's all about the joy of playing and learning from one another.
+    </p>
+    <p>
+      Feel free to join us at the chess club for some enjoyable games. We've got boards and clocks ready for you, but if
+      you prefer your own gear, don't hesitate to bring it along.
+    </p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-items: center;
     align-items: center;
+  }
+
+  .about h1 {
+    font-weight: 500;
+  }
+
+  .about p {
+    margin-top: 1rem;
   }
 }
 </style>
