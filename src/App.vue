fcf46a4c35549f4f03866ed613e59afa59932974
ElPlaso <plahsoekusay@gmail.com>
2023-09-01 14:50:42 +1200

    feat: update title and logo

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="VUW Chess Club" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -54,6 +54,10 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
+}
+
+img {
+  border-radius: 25%;
 }
 
 @media (min-width: 1024px) {
