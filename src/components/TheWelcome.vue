08479e8c6508da53201db1387ebc64c7f17da47b
ElPlaso <66361666+ElPlaso@users.noreply.github.com>
2023-09-01 15:57:05 +1200

    1 add home page (#2)
    * feat: update title and logo
    
    * feat: update welcome message
    
    * feat: update about us content
    
    * feat: update welcome items
    
    add links to discord, online chess teams, and email
    
    * feat: update welcome
    
    * feat: update about section styles
    
    makes it better suited for different screen sizes
    
    * feat: update icon colours
    
    makes colours match text,
    which also handles dark style

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,41 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import DiscordIcon from './icons/IconDiscord.vue'
+import OnlineChessIcon from './icons/IconOnlineChess.vue'
+import ContactIcon from './icons/IconContact.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <DiscordIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Discord</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    Join our <a href="https://vuejs.org/" target="_blank" rel="noopener">discord server</a> to chat with other members of
+    the club and to keep up with the latest news and events.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <OnlineChessIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Online Chess</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
+    Join our <a href="https://www.chess.com/club/vuw-chess-club/join/cac54e">Chess.com club</a> to play online chess with
+    other members of the club. We also have a
+    <a href="https://lichess.org/team/vuw-chess-club" target="_blank" rel="noopener">Lichess.org team</a>.
 
-    <br />
-
-    More instructions are available in <code>README.md</code>.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <ContactIcon />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Contact</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Feel free to contact us at
+    <a href="mailto:vuwchess@gmail.com" type="email" target="_blank" rel="noopener">vuwchess@gmail.com</a> for any
+    questions or queries.
   </WelcomeItem>
 </template>
