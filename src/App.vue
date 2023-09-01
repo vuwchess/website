24000ff84235ae10e512e17b9776a04996c466ef
ElPlaso <plahsoekusay@gmail.com>
2023-09-01 14:52:49 +1200

    feat: update welcome message

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="VUW Chess Club" />
+      <HelloWorld msg="Welcome" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      This is the official website of the Victoria University of Wellington Chess Club.
     </h3>
   </div>
 </template>
