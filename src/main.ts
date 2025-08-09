9c63606cfa1bc59936ae828fee24ed4d4b49115e
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-08-09 20:29:37 +1200

    Add calendar to events page

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
+import { setupCalendar } from 'v-calendar'
+import 'v-calendar/style.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(PrimeVue)
+app.use(setupCalendar, {})
 
 app.mount('#app')
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { Calendar } from 'v-calendar';
+import 'v-calendar/style.css';
+
+import { ref } from 'vue';
+
+const selectedColor = ref('green');
 </script>
 
 <template>
     <div class="events">
         <h1 class="green">Events</h1>
+        <Calendar :color="selectedColor" />
     </div>
 </template>
 
